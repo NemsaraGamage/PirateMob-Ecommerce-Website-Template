40457c15acb7523b2e13546f9681286326d7a398
Nemsara Gamage <113989584+MGNemsara@users.noreply.github.com>
2024-07-02 17:50:01 +0300

    initial steps

---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../components/styles.css';
+
+
+// npm run deploy
+
+const NavBar = () => {
+  return (
+    <>
+    <div className="navbar">
+      <Link to="/" className="logo">MovieBlocks</Link>
+      <div className="nav-links">
+        <Link to="/" className="nav-link">Home</Link>
+        <div className="genre-dropdown">
+          <Link to="#" className="nav-link">Genre</Link>
+          <div className="dropdown-menu">
+            <Link to="/comedy" className="dropdown-item">Comedy</Link>
+            <Link to="#" className="dropdown-item">Horror</Link>
+            <Link to="#" className="dropdown-item">Adventure</Link>
+            <Link to="#" className="dropdown-item">Action</Link>
+            <Link to="#" className="dropdown-item">Thriller</Link>
+            <Link to="#" className="dropdown-item">Mystery</Link>
+            <Link to="#" className="dropdown-item">Crime</Link>
+            <Link to="#" className="dropdown-item">Family</Link>
+            <Link to="#" className="dropdown-item">Drama</Link>
+            <Link to="#" className="dropdown-item">War</Link>
+          </div>
+        </div>
+        <Link to="/toppicks" className="nav-link">Top Picks</Link>
+        <Link to="/tvshow" className="nav-link">TV Shows</Link>
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search..." />
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default NavBar;
---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import '../styles.css';
+import NavBarHead from "../NavBar";
+import FooterNav from "../Footer"
+import { useNavigate } from 'react-router-dom';
+
+const MainPage = () => {
+
+    return (
+        <>
+
+        {/* Nav bar */}
+        <NavBarHead></NavBarHead>
+
+
+
+        </>
+    )
+}
+
+export default MainPage;
