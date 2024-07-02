152c79a3010230ca636740f31180f0ad2dc241f6
Nemsara Gamage <113989584+MGNemsara@users.noreply.github.com>
2024-07-02 20:55:41 +0300

    inital web design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,26 +9,24 @@ const NavBar = () => {
   return (
     <>
     <div className="navbar">
-      <Link to="/" className="logo">MovieBlocks</Link>
+      <Link to="/" className="logo">PirateMob</Link>
       <div className="nav-links">
         <Link to="/" className="nav-link">Home</Link>
         <div className="genre-dropdown">
-          <Link to="#" className="nav-link">Genre</Link>
+          <Link to="#" className="nav-link">Catagories</Link>
           <div className="dropdown-menu">
             <Link to="/comedy" className="dropdown-item">Comedy</Link>
             <Link to="#" className="dropdown-item">Horror</Link>
+            <Link to="#" className="dropdown-item">RPG</Link>
             <Link to="#" className="dropdown-item">Adventure</Link>
             <Link to="#" className="dropdown-item">Action</Link>
             <Link to="#" className="dropdown-item">Thriller</Link>
             <Link to="#" className="dropdown-item">Mystery</Link>
-            <Link to="#" className="dropdown-item">Crime</Link>
             <Link to="#" className="dropdown-item">Family</Link>
-            <Link to="#" className="dropdown-item">Drama</Link>
             <Link to="#" className="dropdown-item">War</Link>
           </div>
         </div>
-        <Link to="/toppicks" className="nav-link">Top Picks</Link>
-        <Link to="/tvshow" className="nav-link">TV Shows</Link>
+        <Link to="/toppicks" className="nav-link">Community</Link>
       </div>
       <div className="search-bar">
         <input type="text" placeholder="Search..." />
--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,15 +4,19 @@ import NavBarHead from "../NavBar";
 import FooterNav from "../Footer"
 import { useNavigate } from 'react-router-dom';
 
+import logo from '../pics/logo.png'
+
 const MainPage = () => {
 
     return (
         <>
-
         {/* Nav bar */}
         <NavBarHead></NavBarHead>
 
-
+        <div className="logoCon">
+            <h3>PirateMob</h3>
+            <img src={logo}></img>
+        </div>
 
         </>
     )
